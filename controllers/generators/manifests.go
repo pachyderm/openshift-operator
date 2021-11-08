a3a429c3302afd09474f9644ce551bfc0eb75e48
Edmund Ochieng <OchiengEd@users.noreply.github.com>
2021-11-08 15:12:45 +0300

    revert patch to use alternate etcd binary path (#12)
    Add change to revert the path of the etcd binary from /bin/etcd  back to the original value of /usr/local/bin/etcd.
    
    Signed-off-by: Edmund Ochieng <ochienged@gmail.com>

--- controllers/generators/manifests.go
+++ controllers/generators/manifests.go
@@ -179,12 +179,6 @@ func (c *PachydermCluster) EtcdStatefulSet() *appsv1.StatefulSet {
 	for i, container := range etcd.Spec.Template.Spec.Containers {
 		if container.Name == "etcd" {
 			etcd.Spec.Template.Spec.Containers[i].Image = catalog.etcdImage().Name()
-			// Patch:  to change the location of the etcd binary
-			for j, arg := range container.Args {
-				if arg == "/usr/local/bin/etcd" {
-					etcd.Spec.Template.Spec.Containers[i].Args[j] = "/bin/etcd"
-				}
-			}
 		}
 	}
 

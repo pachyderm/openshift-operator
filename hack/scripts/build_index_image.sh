4fb3cb4e09200c5dbf62dc23d59cd905fd70b3ae
Edmund Ochieng <ochienged@gmail.com>
2022-01-13 09:59:57 -0600

    switch operator index image to file-based format
    Change operator bundle index image from sqlite-based to new file based
    index image.
    
    Signed-off-by: Edmund Ochieng <ochienged@gmail.com>

---
+++ hack/scripts/build_index_image.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+setup_catalog_directory() {
+    cleanup
+
+    echo "Setup custom index image..."
+    mkdir pachyderm-operator-index
+    opm alpha generate dockerfile pachyderm-operator-index
+    opm init pachyderm-operator --default-channel=stable --description=./README.md --output yaml > pachyderm-operator-index/operator.yaml
+    opm render quay.io/opdev/pachyderm-bundle:${VERSION} --output yaml >> pachyderm-operator-index/operator.yaml
+}
+
+prepare_index_image() {
+cat << EOF >> pachyderm-operator-index/operator.yaml
+---
+schema: olm.channel
+package: pachyderm-operator
+name: stable
+entries:
+- name: pachyderm-operator.v${VERSION}
+EOF
+}
+
+build_index_image() {
+    opm validate ./pachyderm-operator-index
+    if [ $? -eq 0 ]
+    then
+        ${BUILD_TOOL} build -t pachyderm-index:latest -f pachyderm-operator-index.Dockerfile .
+    fi
+}
+
+cleanup() {
+    if [ -d ./pachyderm-operator-index ]
+    then
+        rm -rf ./pachyderm-operator-index ./pachyderm-operator-index.Dockerfile
+    fi
+}
+
+setup_catalog_directory
+prepare_index_image
+build_index_image
+cleanup

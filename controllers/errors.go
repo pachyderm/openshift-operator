6c2602326020d975d268ebf2124737d1475eabda
Edmund Ochieng <ochienged@gmail.com>
2022-07-15 11:30:14 -0500

    Convert the postgresql backup format to tar
    This commit converts the format for backups taken from PostgreSQL
    database instance from flat files to tar backups.
    
    Signed-off-by: Edmund Ochieng <ochienged@gmail.com>

--- controllers/errors.go
+++ controllers/errors.go
@@ -1,10 +1,18 @@
 package controllers
 
-import "errors"
+import (
+	"errors"
+)
 
 var (
 	// ErrServiceNotReady returns when a microservice is not ready
 	ErrServiceNotReady = errors.New("waiting for core microservice")
 	// ErrPasswordNotFound returns when the postgres database password is not found
 	ErrPasswordNotFound = errors.New("postgres-password key not found")
+	// ErrorPachydermNotFound is returned when the pachyderm object is not found in the restore
+	ErrPachydermNotFound = errors.New("pachyderm resource not found")
+	// ErrDatabaseNotFound is returned when the database dump is not found in the restore
+	ErrDatabaseNotFound = errors.New("database restore not found")
+	// ErrPachdPodsRunning is returned when pachd pods are running while in maintenance mode
+	ErrPachdPodsRunning = errors.New("pachd pods still running")
 )

ce112809792d63daf54586e8898a834bb1e43c36
Edmund Ochieng <ochienged@gmail.com>
2022-08-30 10:18:00 -0500

    Split the backup and restore functionality
    Signed-off-by: Edmund Ochieng <ochienged@gmail.com>

--- main.go
+++ main.go
@@ -105,6 +105,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "PachydermExport")
 		os.Exit(1)
 	}
+	if err = (&controllers.PachydermImportReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "PachydermImport")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
